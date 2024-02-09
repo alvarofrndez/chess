7393cc59c13274e486084b903a7b4aaccef6e9a4
Alvaro Fernandez <alvarofdayala@gmail.com>
2024-02-09 18:08:18 +0100

    solucion token, logout, inicio css de login y singn

--- client/src/views/Login/LoginView.vue
+++ client/src/views/Login/LoginView.vue
@@ -19,9 +19,7 @@
 </script> 
 
 <template>
-    <div class='background-image-container'>
-        <img class='background-image' src='/src/assets/images/peon-negro.png'/>
-    </div>
+
     <div class='login-container'>
         <input type='text' name='email' id='email' v-model='email'>
         <input type='text' name='password' id='password' v-model='password'>
@@ -34,22 +32,4 @@
 <style lang='scss'>
     @import '@/assets/style.scss';
 
-    .background-image-container{
-        // size
-        width: 60vw;
-        height: 80vh;
-
-        // position
-        position: absolute;
-
-        // decoration
-        perspective: 700px;
-
-        .background-image{
-            width: 100%;
-            height: 100%;
-            transform-style: preserve-3d;
-            transform: rotateX(45deg);
-        }
-    }
 </style>
